87fd79c82d2b8fd963fb3765f9dde75769a0a1e9
AdluAghnia <adluaghnia03@gmail.com>
2024-02-15 09:11:44 +0700

    restructur codeya

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,44 +2,22 @@ package main
 
 import (
 	"fmt"
-  "latihan.com/latihan/internal/example_slices"
+  "latihan.com/latihan/internal/example"
 )
 
 
-func tipe() {
-  // Jenis-jenis tipe data sederhana di Go
-  var a bool =  true
-  var b int = 5
-  var c float32 = 3.14
-  var d string = "Hi Hallo"
-
-  fmt.Println("Boolean = ", a)
-  fmt.Println("Integer = ", b)
-  fmt.Println("Float = ", c)
-  fmt.Println("String = ", d)
-}
-
-
-func array() {
-  var cars = [4]string{"Volvo", "BMW", "Ford", "Mazda"};
-  harga := [4]int{10, 20, 23, 40};
-  fmt.Println(cars);
-  fmt.Println(harga);
-  fmt.Println("Mobil = ", cars[2]);
-  fmt.Println("Harga = ", harga[2]);
-}
 
 func main() {
   fmt.Println("=====================")
-  array()
+  example.ArrayExample()
   fmt.Println("=====================")
-  tipe()
+  example.ArrayExample()
   fmt.Println("=====================")
-  eslice.Slice()
+  example.Slice()
   fmt.Println("=====================")
-  eslice.AccessSlice()
+  example.AccessSlice()
   fmt.Println("=====================")
-  eslice.AppendSlice()
+  example.AppendSlice()
   fmt.Println("=====================")
-  eslice.ChangeElementSlice()
+  example.ChangeElementSlice()
 }
