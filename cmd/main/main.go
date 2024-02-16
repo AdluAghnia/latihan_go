9bb31aa9ea1fb7a07a8df73a2311f2387b8b555f
AdluAghnia <adluaghnia03@gmail.com>
2024-02-16 09:45:46 +0700

    add : adding Assigment Operator

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,23 +1,10 @@
 package main
 
 import (
-	"fmt"
   "latihan.com/latihan/internal/example"
 )
 
-
-
 func main() {
-  fmt.Println("=====================")
-  example.ArrayExample()
-  fmt.Println("=====================")
-  example.ArrayExample()
-  fmt.Println("=====================")
-  example.Slice()
-  fmt.Println("=====================")
-  example.AccessSlice()
-  fmt.Println("=====================")
-  example.AppendSlice()
-  fmt.Println("=====================")
-  example.ChangeElementSlice()
+  example.AssignmentOperatorsExample()
+  example.ArithmeticExample()
 }
